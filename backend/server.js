f8085a11258022c9efcce64fd74a1893bfeb43bd
nahida <nahida.maqbool@lelafe.com>
2026-01-12 19:15:35 +0530

    first comit after cicd

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ credentials:true
 app.use(express.json());
 
 app.get('/api',(req,res)=>{
-res.send('Api is working');
+res.send('Api is working ,ci cd integration implemented');
 });
 
 app.use('/api/auth', authRoutes);
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+    API_URL: "http://15.207.254.225/api"
+
+  // API_URL: "https://taskmanagment/api"
+};
