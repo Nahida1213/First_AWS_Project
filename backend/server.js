dc3ca418755002c4fb617f024081e84314959097
domain <105207550+Nahida1213@users.noreply.github.com>
2026-01-12 23:55:23 +0530

    create ssh  key using  pem file

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ credentials:true
 app.use(express.json());
 
 app.get('/api',(req,res)=>{
-res.send('Api is working , new ci cd integration ');
+res.send('Api is working , new ci cd integration ::::::::::::::::::::::::::::,test ');
 });
 
 app.use('/api/auth', authRoutes);
